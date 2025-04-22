584c531e611a032a9267fdd9ae55e22611174ab6
Kiki-613 <takr24ig@student.ju.se>
2025-04-22 13:51:26 +0200

    Changes to javascript for input on homepage to make it more coherent

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-document.getElementById("searchButton").addEventListener("click", () => {
-  const query = document.getElementById("searchInput").value.trim();
-  const date = document.getElementById("dateInput").value;
+document.getElementById("searchButton").addEventListener("click", function () {
+  var queryInput = document.getElementById("searchInput");
+  var dateInput = document.getElementById("dateInput");
 
-  // Redirect to results page with query params
-  window.location.href = `results.html?query=${encodeURIComponent(
-    query
-  )}&date=${encodeURIComponent(date)}`;
+  /*trim is to remove blank spaces if user leaves any*/
+  var query = queryInput.value.trim();
+  /*.value is here to get the current value of the input field: found at https://www.shecodes.io/athena/72807-understanding-the-value-property-in-javascript*/
+  var date = dateInput.value;
+
+  var encodedQuery = encodeURIComponent(query);
+  var encodedDate = encodeURIComponent(date);
+
+  var url = "results.html?query=" + encodedQuery + "&date=" + encodedDate;
+  window.location.href = url;
 });
